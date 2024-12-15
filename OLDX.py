335a1198d9d8011b1f5cf1b7f90ecef858345a88
TAMIM-AHMED <153191856+TAMIM-303@users.noreply.github.com>
2024-12-15 21:33:58 +0600

    Update OLDX.py

--- OLDX.py
+++ OLDX.py
@@ -1,1 +1,11 @@
-import OLD
+import os,platform
+os.system('pkg install espeak -y --quiet 2>/dev/null')
+os.system("clear")
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+xd=platform.architecture()[0]
+if xd=="32bit":
+    os.system("clear");exit("\033[91;1m 32Bit Device Not Supported")
+    __import__("OLD")
+elif xd=="64bit":
+    __import__("OLD")
