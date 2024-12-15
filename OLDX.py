cac93ed5c48ea83f7ed7c44e3766523d4300ae40
TAMIM-AHMED <153191856+TAMIM-303@users.noreply.github.com>
2024-12-15 21:43:05 +0600

    Update OLDX.py

--- OLDX.py
+++ OLDX.py
@@ -1,5 +1,3 @@
-import platform,os
-os.system("clear")
-xd = platform.architecture()[0]
-if "64" in xd:import OLD
-else:print(f"The Old clone tool has not yet been released for {xd} It may be available for all bit versions soon.")
+import sys
+sys.path.append('/path/to/OLD')
+import OLD
